eb891ff622109b20b879fcc845ee1df9c90fe643
aditya-abhiram <aditya.abhiram03@gmail.com>
2024-04-29 21:25:19 +0530

    changing to latest

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const clientsecret = process.env.CLIENT_SECRET;
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://project-bridge-4tdny0dz5-aditya-abhirams-projects.vercel.app",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
@@ -110,7 +110,7 @@ app.get(
   "/auth/google/callback",
   async (req, res, next) => {
     passport.authenticate("google", {
-      failureRedirect: "http://localhost:3000/login",
+      failureRedirect: "https://project-bridge-4tdny0dz5-aditya-abhirams-projects.vercel.app/login",
     })(req, res, next);
   },
   async (req, res) => {
@@ -131,7 +131,7 @@ app.get(
     }
 
     if (expectedRole !== userType) {
-      res.redirect("http://localhost:3000/error"); // Redirect to error page if roles mismatch
+      res.redirect("https://project-bridge-4tdny0dz5-aditya-abhirams-projects.vercel.app/error"); // Redirect to error page if roles mismatch
       return;
     }
 
@@ -180,7 +180,7 @@ app.get(
     }
 
     res.redirect(
-      `http://localhost:3000/${
+      `https://project-bridge-4tdny0dz5-aditya-abhirams-projects.vercel.app/${
         userType === "teacher" ? "teachers/TeacherHome" : "students/StudentHome"
       }/${userId}`
     );
@@ -207,7 +207,7 @@ app.get("/logout", (req, res, next) => {
     if (err) {
       return next(err);
     }
-    res.redirect("http://localhost:3000/");
+    res.redirect("https://project-bridge-4tdny0dz5-aditya-abhirams-projects.vercel.app/");
   });
 });
 
